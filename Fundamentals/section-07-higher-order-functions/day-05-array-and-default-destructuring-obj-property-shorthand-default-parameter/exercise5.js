7f3fbc7dd42f76d8044ec65a67ab57380e54caae
Felipe Duarte <feduarte.dev@gmail.com>
2023-06-13 11:05:37 -0300

    daily exercises done

---
+++ Fundamentals/section-07-higher-order-functions/day-05-array-and-default-destructuring-obj-property-shorthand-default-parameter/exercise5.js
@@ -0,0 +1,9 @@
+// Usando array destructuring, armazene cada nome presente na variável moreStudents em variáveis separadas:
+const moreStudents = ['Chris', ['Ahmad', 'Antigoni'], ['Toby', 'Sam']];
+
+// Escreva seu código aqui
+
+const [student1, student2, student3, student4, student5] = moreStudents.flat();
+// const [ student1, [ student2, student3 ], [student4, student5 ] ] = moreStudents; << Gabarito
+
+console.log(student1, student2, student3, student4, student5);

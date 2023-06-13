83fcfccfcc2d5029ca1d8d841a1564bd0b45e6a5
Felipe Duarte <feduarte.dev@gmail.com>
2023-06-13 09:52:38 -0300

    lesson exercises done

--- Fundamentals/section-07-higher-order-functions/day-03-filter-reduce/lessonExercise.js
+++ Fundamentals/section-07-higher-order-functions/day-03-filter-reduce/lessonExercise.js
@@ -1,7 +1,10 @@
+// Para fixar ainda mais o conceito de reduce, faça uma função que some todos os números pares do array numbers. Tente criar duas funções, uma usando reduce e filter, e outra apenas usando reduce.
+
 const numbers = [18, 19, 23, 53, 4, 5, 76, 23, 54];
 
-
-const onlyReduce = numbers.reduce((sum, number) => ((number % 2 === 0) ? sum + number : sum));
+const onlyReduce = numbers.reduce((sum, number) =>
+  number % 2 === 0 ? sum + number : sum
+);
 
 console.log(onlyReduce);
 

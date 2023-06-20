71df538973d24fc6e01d48743d260de62bd633bf
Felipe Duarte <feduarte.dev@gmail.com>
2023-06-20 10:37:28 -0300

    sections 4 - day 2 finished

--- Fundamentals/section-04-javascript-events-web-storage/day-02-elements-sub-itens/lessonExercise3.js
+++ Fundamentals/section-04-javascript-events-web-storage/day-02-elements-sub-itens/lessonExercise3.js
@@ -1,3 +1,4 @@
+// Remova a tag h2, filha do elemento where-are-you.
 const pai = document.querySelector('#where-are-you');
 const filho = document.querySelector('#where-are-you').children
 pai.removeChild(filho[0])
